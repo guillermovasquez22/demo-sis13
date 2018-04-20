27fc3aee3acd8d80024467684d194054248c3cbd
Guillermo Rafael Vásquez Castaneda <memuxit@gmail.com>
2018-04-20 08:28:03 -0600

    Eliminacion de algunas variables, y se descomentaron algunas lineas

--- src/vectores/Ejemplos.java
+++ src/vectores/Ejemplos.java
@@ -5,15 +5,13 @@ import javax.swing.JOptionPane;
 
 public class Ejemplos {
     public static void main(String[] args) {
-        int[] edad;
-        edad = new int[10];
         double[] precio = new double[3];
         double total = 0;
-        /*for (int i = 0; i < precio.length; i++) {
+        for (int i = 0; i < precio.length; i++) {
             precio[i] = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese un precio"));
             total += precio[i];
         }
-        JOptionPane.showMessageDialog(null, "El total es: " + total);*/
+        JOptionPane.showMessageDialog(null, "El total es: " + total);
         int seguir;
         String nombre;
         do {
@@ -22,5 +20,4 @@ public class Ejemplos {
             seguir = JOptionPane.showConfirmDialog(null, "¿Desea continuar?", "Confirmar", JOptionPane.YES_NO_OPTION);
         } while(seguir == JOptionPane.YES_OPTION);
     }
-    
 }
